59af535b275082164ac22af65b18b0b01d12ad62
Met Martinez <zq875328@gmail.com>
2025-06-05 19:04:33 +0900

    fixup! debug print マクロ追加

--- Common/include/Common_DebugPrint.h
+++ Common/include/Common_DebugPrint.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 #define _DEBUG_PRINT_INTERNAL(level, format, ...) \
-    printf("[%s] File <%s>, at line %d, in <%s>: " format "\n", level,__FILE__,__LINE__,__func__, ##__VA_ARGS__)
+        printf("[%s] File <%s>, at line %d, in <%s>: " format "\n", level, __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
 // #define ENABLE_PRINT_ERROR
 // #define ENABLE_PRINT_WARNING
@@ -12,28 +12,28 @@
 
 #ifdef ENABLE_PRINT_DEBUG
 #define PRINT_DEBUG(format, ...) \
-    _DEBUG_PRINT_INTERNAL("DEBUG", format, ##__VA_ARGS__)
+        _DEBUG_PRINT_INTERNAL("DEBUG", format, ## __VA_ARGS__)
 #else
 #define PRINT_DEBUG(...)
 #endif
 
 #ifdef ENABLE_PRINT_INFO
-#define PRINT_INFO(format, ...) \   
-    _DEBUG_PRINT_INTERNAL("INFO", format, ##__VA_ARGS__)
+#define PRINT_INFO(format, ...) \
+        _DEBUG_PRINT_INTERNAL("INFO", format, ## __VA_ARGS__)
 #else
 #define PRINT_INFO(...)
 #endif
 
 #ifdef ENABLE_PRINT_WARNING
 #define PRINT_WARNING(format, ...) \
-    _DEBUG_PRINT_INTERNAL("WARNING", format, ##__VA_ARGS__)
+        _DEBUG_PRINT_INTERNAL("WARNING", format, ## __VA_ARGS__)
 #else
 #define PRINT_WARNING(...)
 #endif
 
 #ifdef ENABLE_PRINT_ERROR
 #define PRINT_ERROR(format, ...) \
-    _DEBUG_PRINT_INTERNAL("ERROR", format, ##__VA_ARGS__)
+        _DEBUG_PRINT_INTERNAL("ERROR", format, ## __VA_ARGS__)
 #else
 #define PRINT_ERROR(...)
 #endif
